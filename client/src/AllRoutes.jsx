b9cea73874a9552a463b2667f451e6476ddf0930
Harsh <aggarwalharsh939@gmail.com>
2022-12-15 17:43:58 +0530

    initial commit

---
+++ client/src/AllRoutes.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Auth from './pages/Home/Auth/Auth'
+import Questions from './pages/Home/Questions/Questions'
+import AskQuestion from './pages/Home/AskQuestion/AskQuestion'
+import DisplayQuestion from './pages/Home/Questions/DisplayQuestion'
+import Tags from './pages/Home/Tags/Tags'
+import Users from './pages/Home/Users/Users'
+import UserProfile from './pages/Home/UserProfile/UserProfile'
+
+
+const AllRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/Auth' element={<Auth />}/>
+            <Route path='/Questions' element={<Questions />}/>
+            <Route path='/AskQuestion' element={<AskQuestion />}/>
+            <Route path='/Questions/:id' element={<DisplayQuestion />}/>
+            <Route path='/Tags' element={<Tags />}/>
+            <Route path='/Users' element={<Users />}/>
+            <Route path='/Users/:id' element={<UserProfile />}/>
+        </Routes>
+    )
+}
+
+export default AllRoutes
